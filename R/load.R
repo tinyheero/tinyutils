2f6bd4cebcfca7cc12f9254f4e3b9a9496168526
Fong Chun Chan <fongchunchan@gmail.com>
2015-07-23 17:59:58 -0700

    Adding header = TRUE in fread()

--- R/load.R
+++ R/load.R
@@ -9,7 +9,7 @@
 #' @return A matrix of the infile
 #' @export
 load_mat <- function(infile){
-	in.dt <- data.table::fread(infile)
+	in.dt <- data.table::fread(infile, header = TRUE)
 	in.dt <- in.dt[!duplicated(in.dt[, 1]), ]
 	in.mat <- as.matrix(in.dt[, -1, with = FALSE])
 	rownames(in.mat) <- unlist(in.dt[, 1, with = FALSE])
