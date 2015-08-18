4da7f3769f2879a837939c615d8e2972eeb613cf
Fong Chun Chan <fongchunchan@gmail.com>
2015-08-17 21:38:27 -0600

    Adding New Function
    New function get_row_by_names() provides a pipe friendly version of
    retrieving a subset of a matrix by rownames.

---
+++ R/get_row_by_names.R
@@ -0,0 +1,15 @@
+#' Get Row of Matrix by Rowname
+#'
+#' This function provides a pipe friendly way of retrieving rows of a matrix
+#' by rownames.
+#'
+#' @param in.mat The input matrix
+#' @param in.row.names The row names to subset by
+#' @return A subset of the matrix
+#' @export
+#' @examples 
+#' mat <- matrix(1:20, 5, 4, dimnames = list(LETTERS[1:5], NULL))
+#' get_row_by_name(mat, c("A", "D", "E"))
+get_row_by_name <- function(in.mat, in.row.names) {
+  in.mat[in.row.names, ]
+}
