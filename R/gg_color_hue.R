547e0bc9921a773a609bdbc0a28da240b051e213
fongchun <fongchunchan@gmail.com>
2015-09-09 21:20:16 +0000

    Adding Function to Generate Default ggplot2 Color Palette

---
+++ R/gg_color_hue.R
@@ -0,0 +1,12 @@
+#' Get the Default ggplot2 Color Palette
+#' 
+#' \code{gg_color_hue} returns the default ggplot color palette for a given
+#' number of colors
+#'
+#' @author John Colby \url{http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette}
+#
+#' @param n Number of Coolors
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
