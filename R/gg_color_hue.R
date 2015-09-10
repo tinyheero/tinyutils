586a956b8b0a02b5d4cb9cb217d055c3ad127fdb
Fong Chun Chan <fongchunchan@gmail.com>
2015-09-09 23:41:06 -0700

    Exporting gg_color_hue and Updating Roxygen

--- R/gg_color_hue.R
+++ R/gg_color_hue.R
@@ -6,6 +6,7 @@
 #' @author John Colby \url{http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette}
 #
 #' @param n Number of Coolors
+#' @export
 gg_color_hue <- function(n) {
   hues = seq(15, 375, length=n+1)
   hcl(h=hues, l=65, c=100)[1:n]
