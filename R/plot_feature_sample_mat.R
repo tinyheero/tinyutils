99fe2b4e348f9aa3d6535591028f7ddfffc9969b
fongchun <fongchunchan@gmail.com>
2015-09-25 15:26:24 -0700

    Adding Different Feature Type Display Mode

--- R/plot_feature_sample_mat.R
+++ R/plot_feature_sample_mat.R
@@ -10,17 +10,31 @@
 #'   in the final plot on the x-axis
 #' @param fill.colors character vector indicating the colors of the different 
 #'   "types". The names should be the types with the value being the color
+#' @param type.display.mode Specify whether multiple or a single type can appear 
+#'   in the same feature/sample cell
+#' @param type.order Specify the "priority" of the types. This has an effect when
+#'   type.display.mode is set to single. 
 #' @export
 #' @examples
-#' v1 <- c("RCOR1", "NCOR1", "LCOR", "RCOR1", "RCOR1")
-#' v2 <- c("sampleA", "sampleC", "sampleB", "sampleC", "sampleA")
-#' v3 <- c("Deletion", "Deletion", "SNV", "Rearrangement", "SNV")
+#' v1 <- c("RCOR1", "NCOR1", "LCOR", "RCOR1", "RCOR1", "RCOR1", "RCOR1")
+#' v2 <- c("sampleA", "sampleC", "sampleB", "sampleC", "sampleA", "sampleC", "sampleC")
+#' v3 <- c("Deletion", "Deletion", "SNV", "Rearrangement", "SNV", "Rearrangement", "SNV")
 #' feature.order <- c("RCOR1", "NCOR1", "LCOR")
 #' sample.id.order <- c("sampleA", "sampleB", "sampleC")
 #' in.dt <- data.table::data.table(feature = v1, sampleID = v2, type = v3)
 #' fill.colors <- c("Deletion" = "Blue", "Rearrangement" = "Green", "SNV" = "Red")
-#' plot_feature_sample_mat(in.dt, feature.order, sample.id.order, fill.colors = fill.colors)
-plot_feature_sample_mat <- function(in.dt, feature.order, sample.id.order, fill.colors) {
+#'
+#' plot_feature_sample_mat(in.dt, feature.order, sample.id.order, 
+#'   fill.colors = fill.colors)
+#'
+#' plot_feature_sample_mat(in.dt, feature.order, sample.id.order, fill.colors = fill.colors,
+#'   type.display.mode = "single")
+#'
+#' plot_feature_sample_mat(in.dt, feature.order, sample.id.order, fill.colors = fill.colors,
+#'   type.display.mode = "single", type.order = c("Rearrangement", "SNV", "Deletion"))
+plot_feature_sample_mat <- function(in.dt, feature.order, sample.id.order, fill.colors,
+                             type.display.mode = c("multiple", "single"), 
+                             type.order) {
 
   # Copy so that it doesn't change the in.dt from the pass-in
   tmp.dt <- data.table::copy(in.dt)
@@ -34,6 +48,22 @@ plot_feature_sample_mat <- function(in.dt, feature.order, sample.id.order, fill.
   if (missing(sample.id.order)) {
     message("Detected no sample.id.order. Setting sample.id.order")
     sample.id.order <- unique(tmp.dt[, sampleID])
+  }
+
+  if (missing(type.order)) {
+    message("Detected no type.order Setting type.order")
+    type.order <- unique(tmp.dt[, type])
+  }
+
+  if (type.display.mode == "single") {
+    tmp.df <- unique(tmp.dt)
+    tmp.df <- dplyr::as_data_frame(tmp.df)
+    tmp.df <- dplyr::mutate(tmp.df, 
+                            type = factor(type, levels = rev(type.order)))
+    tmp.df <- dplyr::group_by(tmp.df, sampleID, feature)
+    tmp.df <- dplyr::arrange(tmp.df, type)
+    tmp.df <- dplyr::top_n(tmp.df, n = 1)
+    tmp.dt <- data.table(tmp.df)
   }
 
   tmp.dt <- tmp.dt[, feature := as.numeric(factor(feature, 
@@ -63,4 +93,3 @@ plot_feature_sample_mat <- function(in.dt, feature.order, sample.id.order, fill.
   }
   p1
 }
-
